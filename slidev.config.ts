993a49e4255b0a8b73f0bcae3794c1798ae1a156
AlchemistX <hick4321@gmail.com>
2025-09-04 12:14:32 +0900

    initial commit

---
+++ slidev.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@slidev/cli'
+
+export default defineConfig({
+  css: 'unocss',
+  highlighter: 'shiki',
+  lineNumbers: false,
+  monaco: true,
+  remoteAssets: false,
+  selectable: false,
+  theme: 'default',
+  themeConfig: {},
+})
